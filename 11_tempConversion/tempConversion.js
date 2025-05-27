fc8cf0c14ae3b6336b3ed353a16f76207c22eb4d
Tranquil Tortoise <SlinkTheTranquilTortoise@gmail.com>
2025-05-27 00:18:07 -1000

    complete 11_tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesF) {
+  let temp = (degreesF - 32) * (5 / 9);
+  temp = Math.round(temp * 10) / 10;
+  return temp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesC) {
+  let temp = degreesC * (9/5) + 32;
+  temp = Math.round(temp * 10) / 10;
+  return temp;
 };
 
 // Do not edit below this line
