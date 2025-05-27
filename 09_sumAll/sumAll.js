80b570533921863a9495d7e0a85439336701ee90
Tranquil Tortoise <SlinkTheTranquilTortoise@gmail.com>
2025-05-26 23:54:07 -1000

    complete 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
 
+    if (Number.isInteger(numOne) === false || typeof numOne != 'number' || numOne < 0) {
+        return 'ERROR'
+    }
+    if (Number.isInteger(numTwo) === false || typeof numTwo != 'number' || numTwo < 0) {
+        return 'ERROR'
+    }
+
+    let sum = 0;
+    const arr = [];
+
+    if (numOne <= numTwo) {
+        for (let i = numOne; i <= numTwo; i++) {
+            arr.push(i);
+        }
+        } else {
+
+        for (let i = numOne; i >= numTwo; i--) {
+            arr.push(i);
+        }
+    }
+    arr.forEach( (element) => {
+        sum = sum + element;
+    })
+    return sum;
 };
 
 // Do not edit below this line
