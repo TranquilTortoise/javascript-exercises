1ef4e96ee2f0dab86fa27848860e7f1ef7d0bd23
Tranquil Tortoise <SlinkTheTranquilTortoise@gmail.com>
2025-05-26 22:56:15 -1000

    complete 07_reverse a String

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(str) {
 
+    // solution: return string.split("").reverse().join("");
+    
+    const arr = [...str];
+
+    let reversed = "";
+
+    for (i = str.length - 1; i >= 0; i--) {
+        reversed = reversed + arr[i];
+    }
+
+    return reversed;
 };
 
 // Do not edit below this line
