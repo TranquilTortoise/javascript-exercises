f266fcf8cf09420aadc87814a70680d6df1e2d06
Tranquil Tortoise <SlinkTheTranquilTortoise@gmail.com>
2025-05-26 23:19:35 -1000

    complete 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr, ...theArgs) {
+
+    return arr.filter(item => 
+        !theArgs.includes(item));
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
