18192c492504f4f1cfc40bd705df4c74b215fe11
Tranquil Tortoise <SlinkTheTranquilTortoise@gmail.com>
2025-05-26 22:42:44 -1000

    complete 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+function repeatString(str, num) {
 
-};
+    let repeatedString = "";
+
+    if (num < 0) {return "ERROR";}
+
+    for (i = 0; i < num; i++) {
+        repeatedString = repeatedString + str;
+    }
+
+    return repeatedString;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
