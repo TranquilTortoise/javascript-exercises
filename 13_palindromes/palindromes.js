b5085b02f0f5d612e14b4f9de1000906a03dfb04
Tranquil Tortoise <SlinkTheTranquilTortoise@gmail.com>
2025-06-03 21:56:02 -1000

    completed fibonacci.js

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    const cleanStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+
+    const charArr = [...cleanStr];
+    const reversedArr = [...charArr].reverse();
+
+    const reversedStr = reversedArr.join("");
+
+    if (reversedStr == cleanStr) {return true;}
+
+    return false;
 };
 
 // Do not edit below this line
