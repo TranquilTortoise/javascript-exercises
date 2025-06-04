12fea18724e692f3f8d1a8df67d0f15fa2b534ee
Tranquil Tortoise <SlinkTheTranquilTortoise@gmail.com>
2025-06-03 21:00:12 -1000

    complete calculator.js

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,24 +1,53 @@
-const add = function() {
+const add = function(...theArgs) {
+
+  return theArgs.reduce((sum, currentItem) => {
+    return sum + currentItem;
+  }, 0);
+};
+
+const subtract = function(firstArg, ...theArgs) {
+
+  return theArgs.reduce((difference, currentItem) => {
+    return difference - currentItem;
+  }, firstArg);
 	
 };
 
-const subtract = function() {
+const sum = function(arr) {
+
+  let sum = 0;
+
+  arr.forEach((element) => {
+    sum = sum + element;
+  });
+  return sum;
+};
+
+const multiply = function(arr) {
+
+  let total = 1;
+
+  arr.forEach((element) => {
+    total = total * element;
+  });
+  return total;
+};
+
+const power = function(num, power) {
+
+  let answer = 1
 	
+  for (let i = 0; i < power; i++) {
+    answer = answer * num;
+  }
+
+  return answer;
 };
 
-const sum = function() {
-	
-};
+const factorial = function(num) {
+  if (num === 0 || num === 1) { return 1; }
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+  return num * factorial(num-1);
 	
 };
 
